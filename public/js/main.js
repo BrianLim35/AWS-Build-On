b2d91c3381fba64d7baf2db5be081a82c9442e26
brian <brianlim35@gmail.com>
2022-09-28 16:41:58 +0800

    style and js for poll

--- public/js/main.js
+++ public/js/main.js
@@ -69,3 +69,56 @@ $('#posterUpload').on('change', function(){
     });
 });
 
+let poll = {
+    question: "What do you think of this outfit?",
+    answers: ["Looks Good!", "Looks Meh"],
+    pollCount: 20,
+    answersWeight: [15, 5],
+    selectedAnswer: -1,
+  };
+  
+  let pollDOM = {
+    question: document.querySelector(".poll .question"),
+    answers: document.querySelector(".poll .answers"),
+  };
+  
+  pollDOM.question.innerText = poll.question;
+  pollDOM.answers.innerHTML = poll.answers
+    .map(function (answer, i) {
+      return `
+      <div class="answer" type="button" onclick="markAnswer('${i}')">
+        ${answer}
+        <span class="percentage-bar"></span>
+        <span class="percentage-value"></span>
+      </div>
+      `;
+    })
+    .join("");
+  
+  function markAnswer(i) {
+    poll.selectedAnswer = +i;
+    try {
+      document
+        .querySelector(".poll .answers .answer .selected")
+        .classList.remove("selected");
+    } catch (msg) {}
+    document
+      .querySelectorAll(".poll .answers .answer")
+      [+i].classList.add("selected");
+    showResults();
+  }
+  
+  function showResults() {
+    let answers = document.querySelectorAll(".poll .answers .answer");
+    for (let i = 0; i < answers.length; i++) {
+      let percentage = 0;
+      if (i == poll.selectedAnswer) {
+        percentage = Math.round(
+          ((poll.answersWeight[i] + 1) * 100) / (poll.pollCount + 1)
+        );
+      }
+  
+      answers[i].querySelector(".percentage-bar").style.width = percentage + "%";
+      answers[i].querySelector(".percentage-value").innerText = percentage + "%";
+    }
+  }
